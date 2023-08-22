ec559b6a829e10d89a3cbf1048c9323811c01931
ale4287 <alejandro.algarin@hotmail.com>
2023-08-22 20:13:36 -0300

    Python - laboratorio (1.5-1.6)
    Clase 1 - cuarto semestre

--- Python/Tecnicatura3Py/Leccion07/capa_datos_persona/menu_app_usuario.py
+++ Python/Tecnicatura3Py/Leccion07/capa_datos_persona/menu_app_usuario.py
@@ -1,26 +1,37 @@
-from Leccion007.capa_datos_persona.Usuario import Usuario
-from Leccion007.capa_datos_persona.Usuario_dao import UsuarioDao
+from capa_datos_persona.usuario_dao import UsuarioDAO
 from logger_base import log
+from Usuario import Usuario
 
 opcion = None
 while opcion != 5:
-    print("Opcion: ")
-    print('1-Listar usuarios')
-    print('2-Agregar usuario')
-    print('3-Modificar usuario')
-    print('4-Eliminar usuario')
-    print(('5-Salir'))
+    print('Opciones: ')
+    print('1. Listar Usuarios')
+    print('2. Agregar Usuario')
+    print('3. Modificar Usuario')
+    print('4. Eliminar Usuario')
+    print('5. Salir')
     opcion = int(input('Digite la opcion (1-5): '))
-    if opcion ==1:
-        usuarios = UsuarioDao.seleccionar()
+    if opcion == 1:
+        usuarios = UsuarioDAO.seleccionar()
         for usuario in usuarios:
             log.info(usuario)
-
-    elif opcion ==2:
-        username_var =input('Digite el nombre de usuario: ')
+    elif opcion == 2:
+        username_var = input('Digite el nombre de usuario: ')
         password_var = input('Digite su contraseña: ')
-        usuario= Usuario(username=username_var, password=password_var)
-        usuario_insertado = UsuarioDao.insertar(usuario)
-        log.INFO(f'Usuario insertado: {usuario_insertado}')
+        usuario = Usuario(username=username_var, password=password_var)
+        usuario_insertado = UsuarioDAO.insertar(usuario)
+        log.info(f'Usuario insertado: {usuario_insertado}')
+    elif opcion == 3:
+        id_usuario_var = int(input('Digite el id del usuario a modificar: '))
+        username_var = input('Digite el nombre del usuario a modificar: ')
+        password_var = input('Digite la contraseña del usuario a modificar: ')
+        usuario = Usuario(id_usuario=id_usuario_var, username=username_var, password=password_var)
+        usuario_actualizado = UsuarioDAO.actualizar(usuario)
+        log.info(f'Usuario actualizado: {usuario_actualizado}')
+    elif opcion == 4:
+        id_usuario_var = int(input('Digite el id del usuario a eliminar: '))
+        usuario = Usuario(id_usuario=id_usuario_var)
+        usuario_eliminado = UsuarioDAO.eliminar(usuario)
+        log.info(f'Usuario eliminado: {usuario_eliminado}')
 else:
-        log.info("Salimos de la aplicación, hasta pronto")
+    log.info('Salimos de la aplicacion, Hasta pronto!!!')
