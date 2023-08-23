da0a4d4af57f1050c9ce8e5de3eaeab058ca881b
Rasta07 <fernando.barrionuevo@outlook.com>
2023-08-23 19:56:52 -0300

    Metodología Clase 1 - Video 8
    Metodología Clase 1 - Video 8

--- Java/SistemaEstudiantes/src/main/java/UTN/presentacion/SistemaEstudiantesApp.java
+++ Java/SistemaEstudiantes/src/main/java/UTN/presentacion/SistemaEstudiantesApp.java
@@ -6,23 +6,141 @@ import java.util.Scanner;
 
 public class SistemaEstudiantesApp {
     public static void main(String[] args) {
-        var salir = false; 
+        var salir = false;
         var consola = new Scanner(system.in);
         //se crea una instancia de la clase servicio
-        var estudianteDao =  new EstudianteDao();
-        while(!salir){
+        var estudianteDao = new EstudianteDao();//Esta instancia debe hacerse una vez
+        while (!salir) {
             try {
-                mostrarMenu();
-                salir = ejecutarOpciones(consola,estudianteDao);
-            } catch(Exception e){
-                System.out.println("Ocurrio un error al ejecutar la operacion: "+e.getMessage());
+                mostrarMenu();//Mostramos el menu
+                //Este sera el metodo ejecutarOpciones que devolvera un booleano
+                salir = ejecutarOpciones(consola, estudianteDao);//Este arroja una exception
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error al ejecutar la operacion: " + e.getMessage());
             }
-        }
-        if (conexion != null) {
-            System.out.println("Conexión exitosa: " + conexion);
-        } else {
-            System.out.println("Ocurrió un error al conectarse.");
-        }
 
-    } // Fin main
-} // Fin clase Main
+        } // Fin while
+    }// Fin Main
+
+    private static void mostrarMenu() {
+        System.out.println("""
+                ******** Sistema de Estudiantes *******
+                1.Listar Estudiantes
+                2.Buscar Estudiantes
+                3.Agregar estudiante
+                4.Modificar Estudiante
+                5.Eliminar Estudiante
+                6.Salir
+                Elige una opción:
+                """);
+    }
+
+    //Método para ejecutar las opciones, va a regresar un valor booleano, ya que es el que
+    // puede modificar el valor de la variable salir, si es verdadero termina el ciclo while
+    private static boolean ejecutarOpciones(Scanner consola, EstudianteDAO estudianteDAO) {
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        switch (opcion) {
+            case 1 -> {//Listar estudiantes
+                System.out.println("Listado de Estudiantes...");
+                //no muestra la información, solo recupera la info y regresa una lista
+                var estudiantes = estudianteDAO.listarEstudiantes();//recibe el listado
+                //vamos a iterar cada objeto de tipo estudiante
+                estudiantes.forEach(System.out::println);//para imprimir la lista
+            }//Fin caso 1
+            case 2 -> {//Buscar estudiante por id
+                System.out.println("Introduce el id_estudiante a buscar: ");
+                var idEstudiante = Integer.parseInt(consola.nextLine());
+                var estudiante = new Estudiante(idEstudiante);
+                var encontrado = estudianteDAO.buscarEstudiantePorId(estudiante);
+                if (encontrado)
+                    System.out.println("Estudiante encontrado: " + estudiante);
+                else
+                    System.out.println("Estudiante no encontrado: " + estudiante);
+
+            }//Fin caso 2
+
+            case 3 -> {// Agregar estudiante
+                System.out.println("Agregar estudiante: ");
+                System.out.println("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.println("Apellido: ");
+                var apellido = consola.nextLine();
+                System.out.println("Telefono: ");
+                var telefono = consola.nextLine();
+                System.out.println("Email: ");
+                var email = consola.nextLine();
+                //crear objeto estudiante(sin id)
+                var estudiante = new Estudiante(nombre, apellido, telefono, email);
+                var agregado = estudianteDAO.agregarEstudiante(estudiante);
+                if (agregado)
+                    System.out.println("Estudiante agregado: " + estudiante);
+                else
+                    System.out.println("Estudiante NO agregado: " + estudiante);
+
+
+            }//Fin caso 3
+            case 4 -> {//Modificar estudiante
+                System.out.println("modificar estudiante");
+                // Aqui lo primero es especificar cual es el id del objeto a modificar
+                System.out.println("Id estudiante");
+                var idEstudiante = Integer.parseInt(consola.nextLine());
+                System.out.println("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.println("Apellido: ");
+                var apellido = consola.nextLine();
+                System.out.println("Telefono: ");
+                var telefono = consola.nextLine();
+                System.out.println("Email: ");
+                var email = consola.nextLine();
+                // crear objeto estudiante(sin id)
+                var estudiante = new Estudiante(nombre, apellido, telefono, email);
+                var agregado = estudianteDAO.agregarEstudiante(estudiante);
+                if (agregado)
+                    System.out.println("Estudiante agregado: " + estudiante);
+                else
+                    System.out.println("Estudiante NO agregado" + estudiante);
+
+            }//Fin caso 3
+            case 4 -> {//Modificar estudiante
+                System.out.println("modificar estudiante: ");
+                //Aqui lo primero es especificar cual es el id del objeto a modificar
+                System.out.println("Id Estudiante");
+                var idEstudiante = Integer.parseInt(consola.nextLine());
+                System.out.println("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.println("Apellido: ");
+                var apellido = consola.nextLine();
+                System.out.println("Telefono: ");
+                var telefono = consola.nextLine();
+                System.out.println("Email: ");
+                var email = consola.nextLine();
+
+            }//Fin caso 4
+            case 5 -> {//Eliminar estudiante
+                System.out.println("Eliminar estudiante: ");
+                System.out.println("Id Estudiante");
+                var idEstudiante = Integer.parseInt(consola.nextLine());
+                var estudiante = new Estudiante(idEstudiante);
+                var eliminado = estudianteDAO.eliminarEstudiante(estudiante);
+                if (eliminado)
+                    System.out.println("Estudiante eliminado: " + estudiante);
+                else
+                    System.out.println("Estudiante NO eliminado: " + estudiante);
+            }//Fin caso 5
+            case 6 -> {//salir
+                System.out.println("Hasta pronto !!!");
+                salir = true;
+
+            }//Fin caso 6
+            default -> System.out.println("Opcion no reconocida, ingrese otra opcion");
+        }//Fin switch
+        return salir;
+    }
+
+}// Fin clase
+
+
+
+
+
