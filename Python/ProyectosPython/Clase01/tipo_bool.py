9ea81eb5fe7e62d0e370519375c37565a6468fcc
Maximiliano Chalin <mchalin@gmail.com>
2023-09-16 18:28:01 -0300

    Sentencia de control y booleanos
    Python Clase 3 Video 5

---
+++ Python/ProyectosPython/Clase01/tipo_bool.py
@@ -0,0 +1,15 @@
+#
+# Falta código de issues anteriores.
+#
+
+# mchalin (#56)
+# En sentencia de control, es false cuando es None, true para los demás valores
+if bool(''):
+    print('Es true')
+else:
+    print('Es false')
+
+if bool(' '):
+    print('Es true')
+else:
+    print('Es false')
