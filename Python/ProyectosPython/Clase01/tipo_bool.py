92b6c791dc3d2db6335c513efd57d63e079ebf6b
Rasta07 <fernando.barrionuevo@outlook.com>
2023-09-18 18:57:47 -0300

    PYTHON - CLASE 3 Video 4 Colecciones
    PYTHON - CLASE 3 Video 4 Colecciones

--- Python/ProyectosPython/Clase01/tipo_bool.py
+++ Python/ProyectosPython/Clase01/tipo_bool.py
@@ -6,9 +6,49 @@ print(f'valor: {valor}, Resultado:¨{resultado}')
 
 valor= 0.1
 resultado = bool(valor)
-print(f'valor: {valor}, Resultado: {resultado}')#
-# Falta código de issues anteriores.
-#
+print(f'valor: {valor}, Resultado: {resultado}')
+
+
+
+#Tipo string -> False '', True demas valores
+valor=''
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor='Hola'
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+#Tipo colecciones ->False para colecciones vacias
+#Tipo colecciones -> True para todas las demas
+#Lista
+valor = []
+resultado = bool(valor)
+print(f'valor de una lista vacia: {valor}, Resultado:{resultado}')
+
+valor = [2,3,4]
+resultado, = bool(valor)
+print(f'valor de una lista con elementos: {valor}, Resultado:{resultado}')
+
+#Tupla
+valor=()
+resultado= bool(valor)
+print(f'valor de una tupla vacia: {valor}, Resultado:{resultado}')
+
+valor=(5,)
+resultado= bool(valor)
+print(f'valor de una tupla con elementos: {valor}, Resultado:{resultado}')
+
+#Diccionario
+valor={}
+resultado= bool(valor)
+print(f'valor de un diccionario vacio: {valor}, Resultado:{resultado}')
+
+valor={'Nombre':'Juan','Apellido':'Perez'}
+resultado= bool(valor)
+print(f'valor de un diccionario con elementos: {valor}, Resultado:{resultado}')
+
+
 
 # mchalin (#56)
 # En sentencia de control, es false cuando es None, true para los demás valores
@@ -39,3 +79,6 @@ while variable:
     break
 else:
     print('Es false')
+
+
+
