c978b6ebbc2f84b692934f30ce6e9402fc36d238
dario <sergiodariovega@gmail.com>
2023-09-26 01:18:10 -0300

    update

--- Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -9,22 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import utn.estudiantes.servicio.EstudianteServicio;
 
 @SpringBootApplication
-public class EstudiantesApplication implements CommandLineRunner {
+public class EstudiantesApplication implements CommandLineRunner { // CommandLIneRunner establece que es una aplicación por consola
 
-
-    
     @Autowired
     private EstudianteServicio estudianteServicio;
-    private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class); // Logger imprime información en pantalla
 
-    // Devuelve el caracter de salto de linea en cualquier sistema operativo.
-    String nl = System.lineSeparator();
+    String nl = System.lineSeparator(); // Devuelve el caracter de salto de linea en cualquier sistema operativo.
 
     public static void main(String[] args) {
         logger.info("Iniciando la aplicación...");
         // Inicia la fábrica de Spring
         SpringApplication.run(EstudiantesApplication.class, args);
-        logger.info("Aplicación finalizada.");
+        logger.info("Aplicación finalizada!");
     }
 
     @Override
