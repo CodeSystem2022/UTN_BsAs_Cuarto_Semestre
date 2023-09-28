c48f06d58d9bfab06f6c50745504ca6394b2b388
Maximiliano Chalin <mchalin@gmail.com>
2023-09-28 18:18:08 -0300

    Correccion Issue #82
    Se instala node y librerias para integrar mercadopago
    se corrigen inconsistencias en archivos anteriores para el correcto funcionamiento
    TEST OK

--- JavaScript/e-commerce2022/client/js/products.js
+++ JavaScript/e-commerce2022/client/js/products.js
@@ -4,28 +4,28 @@ const productos = [
     productName: "Banana",
     price: 480,
     quanty: 1,
-    img: "/client/media/Banana.jpg",
+    img: "/media/Banana.jpg"
   },
   {
     id: 2,
     productName: "Leche",
     price: 950,
     quanty: 1,
-    img: "/client/media/Leche.jpg"
+    img: "/media/Leche.jpg"
   },
   {
     id: 3,
     productName: "Pollo",
     price: 750,
     quanty: 1,
-    img: "/client/media/pollo.jpg"
+    img: "/media/pollo.jpg"
   },
   {
     id: 4,
     productName: "Mayonesa",
     price: 550,
     quanty: 1,
-    img: "/client/media/mayonesa.jpg"
+    img: "/media/mayonesa.jpg"
   }
 ]
 
